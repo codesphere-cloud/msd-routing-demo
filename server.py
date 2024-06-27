e8ffc1f3c47dcd9da745fc308ba78dd9a429a406
uekici <umut.ekici@codesphere.com>
2024-06-27 16:52:40 +0200

    made example more flexible

---
+++ server.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import os
+
+replica = os.environ.get('CS_REPLICA')
+server = os.environ.get('SERVER_NAME')
+port = int(os.environ.get('PORT'))
+
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(
+            f'Response:{server}:{replica}:{self.path}'.encode('utf-8')
+        )
+
+with socketserver.TCPServer(('', port), RequestHandler) as httpd:
+    print(f'Serving at port: {port}')
+    httpd.serve_forever()
